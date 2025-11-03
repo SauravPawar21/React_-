0545e824de92525bca7e5a080c82757c54c90002
saurav <pawarsaurav957@gmail.com>
2025-11-03 18:09:37 +0530

    foundation

--- App.js
+++ App.js
@@ -1,22 +1,61 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent=React.createElement(
-    "div",
-    {id:"parent"},
-   [ React.createElement(
-        "div",
-        {id:"child"},
-       [React.createElement("h1",{},"I'm an H1 tag!"),React.createElement("h1",{},"I'm an H1 tag!")]
-    ),React.createElement(
-        "div",
-        {id:"child"},
-       [React.createElement("h1",{},"I'm an H1 tag!"),React.createElement("h1",{},"I'm an H1 tag!")]
-    )]
-)
+//jsx (Transpiled before it reaches the JS) - parcel - Bable
+
+//JSX ==> Babel transpiltes it ot React.createElement ==> ReactElement- JS Obj. ==> HTMLElement(render)
+//React.CreateElement
+
+// const heading = React.createElement("h1",{id:"heading"},"Namaste React");
 
 
-const hedaing = React.createElement("h1",{id:"heading"},"Hellow world in React!");
-console.log(hedaing);//object
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+// const jsxHeading=(
+// <h1 className="head" tabIndex={5}>
+//     This is jsx code
+//     </h1>
+// );
+
+
+
+// const root =ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+
+// console.log(jsxHeading);    
+
+//React Coomponents
+//1.Class based component.
+//2.Functional Component.
+
+
+const ele=<span>React element</span>
+const number = 10000;
+
+const Title = () =>{
+    return(
+    <h1 className="Title">
+        Title comeponet {number}
+        {ele}
+    </h1>
+    )
+}
+const HeadingComponent=()=>{
+    return (
+    <div id="container">
+        <Title/>
+        <h1>namste functional componet</h1>
+    </div>
+    )
+};
+
+//we can also write like that if we have one line only
+const fn = () => true;
+
+const fn2=()=>{
+    return true;
+}
+
+const root =ReactDOM.createRoot(document.getElementById("root"));
+
+//how we render the componets
+root.render(<HeadingComponent/>)
